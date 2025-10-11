dacd98a5fa400d13a2301858d4e035d4ef6ff552
Hovhannes Galstyan <hovogalstyan84@gmail.com>
2025-10-12 02:20:38 +0400

    created endpoint for support (forms) and created initial express app

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,13 @@ const app = express();
 const PORT = 4200;
 
 app.use(cors());
+app.use(express.json());
+
+app.post("/support/", (req, res) => {
+  // const { name, phone } = req.body;
+
+  res.status(200).send({ message: "success" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
